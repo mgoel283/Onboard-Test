2845dfd330129d9475d228970adc3acacdf6012e
FAIR FARE <fairfare@fairs-air.home>
2018-06-14 14:02:40 -0400

    Initial Commit

---
+++ Onboard Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Onboard Test
+//
+//  Created by FAIR FARE on 6/14/18.
+//  Copyright © 2018 FAIR FARE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
